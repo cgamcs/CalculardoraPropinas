f4608ec0425b2f781ee88f23bd78280093d9d0fe
César García <cesar.garciamcs@outlook.com>
2025-07-06 13:29:59 -0600

    consultando la API

--- js/app.js
+++ js/app.js
@@ -46,9 +46,21 @@ function guardarCliente() {
 
     // Mostrar las secciones
     mostrarSecciones()
+
+    // Obtener platillos de la API
+    obtenerPlatillos()
 }
 
 function mostrarSecciones() {
     const seccionesOcultas = document.querySelectorAll('.d-none')
     seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'))
+}
+
+function obtenerPlatillos() {
+    const url = 'http://localhost:4000/platillos'
+
+    fetch(url)
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(error => console.log(error))
 }
