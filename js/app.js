b4532a309b3d080b412a764e050dbe594b73bf5c
César García <cesar.garciamcs@outlook.com>
2025-07-09 19:53:30 -0600

    agregar un producto al pedido

--- js/app.js
+++ js/app.js
@@ -118,5 +118,17 @@ function mostrarPlatillos(platillos) {
 }
 
 function agregarPlatillo(producto) {
-    console.log(producto)
+    // Extraer el pedido actual
+    let { pedido } = cliente
+
+    // Revisar que la cantidad sea mayor a 0
+    if(producto.cantidad > 0) {
+        cliente.pedido = [...pedido, producto]
+
+        console.log(cliente.pedido)
+
+        return
+    }
+
+    console.log(cliente.pedido)
 }
