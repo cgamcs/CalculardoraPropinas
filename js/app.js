728d38108a89aff4e48c677a6eef5ef1a04a1f8e
César García <cesar.garciamcs@outlook.com>
2025-07-10 19:04:04 -0600

    mostrando resultados del resumen

--- js/app.js
+++ js/app.js
@@ -145,5 +145,37 @@ function agregarPlatillo(producto) {
         cliente.pedido = [...resultado]
     }
 
-    console.log(cliente.pedido)
+    // Mostrar resumen
+    actualizarResumen()
+}
+
+function actualizarResumen() {
+    const contenido = document.querySelector('#resumen .contenido')
+    
+    const resumen = document.createElement('DIV')
+    resumen.classList.add('col-md-6')
+
+    // Informacion de la mesa
+    const mesa = document.createElement('P')
+    mesa.textContent = 'Mesa: '
+    mesa.classList.add('fw-bold')
+
+    const mesaSpan = document.createElement('SPAN')
+    mesaSpan.textContent = cliente.mesa
+    mesaSpan.classList.add('fw-normal')
+
+    // Informacion de la hora
+    const hora = document.createElement('P')
+    hora.textContent = 'Hora: '
+    hora.classList.add('fw-bold')
+
+    const horaSpan = document.createElement('SPAN')
+    horaSpan.textContent = cliente.hora
+    horaSpan.classList.add('fw-normal')
+
+    mesa.appendChild(mesaSpan)
+    hora.appendChild(horaSpan)
+
+    contenido.appendChild(mesa)
+    contenido.appendChild(hora)
 }
